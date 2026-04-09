2af7161786e8796d0be6e963941aa689ab67b29f
viethung213 <165547346+viethung213@users.noreply.github.com>
2026-04-09 08:32:39 +0700

    Fix api get parking_slot public

--- src/app/api/parking_slots.py
+++ src/app/api/parking_slots.py
@@ -20,9 +20,7 @@ async def create_parking_slot(request: Request, payload: ParkingSlotCreate, db: 
 
 @router.get("/", response_model=list[ParkingSlotResponse])
 async def get_all_parking_slots(request: Request, db: AsyncSession = Depends(get_db)):
-    user_payload = request.state.user
-    user_id = user_payload.get("sub")
-    return await parking_slots_services.get_parking_slots(db, user_id)
+    return await parking_slots_services.get_parking_slots(db)
 
 @router.put("/", response_model=ParkingSlotResponse)
 async def update_parking_slot(request: Request, payload: ParkingSlotUpdate, db: AsyncSession = Depends(get_db)):
